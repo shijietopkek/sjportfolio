fef25d83c23f6a8807141a338df46b5bb932d2e4
shijietopkek <yu.shijie@dhs.sg>
2020-05-07 00:53:39 +0800

    Updates

---
+++ precache-manifest.35db8f9bad8124eeb35f74b2725d77d8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda463e350015b5ed3d23c83900cecc7",
+    "url": "/sjportfolio/index.html"
+  },
+  {
+    "revision": "e1009003aca3fe604bbf",
+    "url": "/sjportfolio/static/css/main.49a2deb5.chunk.css"
+  },
+  {
+    "revision": "c133ad9f05b55c79f844",
+    "url": "/sjportfolio/static/js/2.002528b2.chunk.js"
+  },
+  {
+    "revision": "32417fa8c54bb2c0a2649c380bf3d7ca",
+    "url": "/sjportfolio/static/js/2.002528b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1009003aca3fe604bbf",
+    "url": "/sjportfolio/static/js/main.a557302a.chunk.js"
+  },
+  {
+    "revision": "fca176567376f538bcc1",
+    "url": "/sjportfolio/static/js/runtime-main.571b5122.js"
+  },
+  {
+    "revision": "81400a384a7b2e31f39cd74fd93d8868",
+    "url": "/sjportfolio/static/media/Me.81400a38.jpg"
+  },
+  {
+    "revision": "ac8d8a0274aee7886107e1565cc773da",
+    "url": "/sjportfolio/static/media/README.ac8d8a02.md"
+  },
+  {
+    "revision": "85033a2721537c0804ea0ef09bb42b9d",
+    "url": "/sjportfolio/static/media/ai.85033a27.jpg"
+  },
+  {
+    "revision": "f2f2b53451bc299d99b7887bd07ca542",
+    "url": "/sjportfolio/static/media/bigdata.f2f2b534.jpg"
+  },
+  {
+    "revision": "3a5eb99bb94f17f4d4c05697d38b80c2",
+    "url": "/sjportfolio/static/media/blockchain.3a5eb99b.jpg"
+  },
+  {
+    "revision": "827c49fce2a4e86a8a3d12c200a73dbd",
+    "url": "/sjportfolio/static/media/favicon.827c49fc.ico"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sjportfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fa1ded1ed7c11438a9b0385b1e112850",
+    "url": "/sjportfolio/static/media/robots.fa1ded1e.txt"
+  },
+  {
+    "revision": "7008f398abab3d53835730a7632d0773",
+    "url": "/sjportfolio/static/media/webdevelopment.7008f398.jpeg"
+  }
+]);
